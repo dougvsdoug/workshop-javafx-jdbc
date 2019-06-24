ebbb9b028bcd2690ecd9d92a9685f69547bc223b
dougvsdoug <dougvsdoug@gmail.com>
2019-06-24 11:58:50 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,42 +1,72 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {// Action é o evento padrão do controller
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {// Action é o evento padrão do controller
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {// Action é o evento padrão do controller
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
-	
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+	public synchronized void loadView(String absoluteName) {// synchronized garante q o processamento do 
+		// método não será interrompido durante o multi-threading
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene =  Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);// main menu recebe o primeiro filho do mainVbox
+			// q é o menu q fica na parte superior da janela principal
+			mainVBox.getChildren().clear();
+			
+			//agora vamos criar a nova tela nas linhas abaixo
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			//manipulamos a cena principal incluindo nela o main menu e os filhos da janela q estamos abrindo
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
